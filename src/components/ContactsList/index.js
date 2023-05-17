88dd7b76d61910d4e26b9f4ef6d9505996280ff3
FihCapua <fihcapua@gmail.com>
2023-05-16 21:35:26 -0300

    feat: create contacts list component and style

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './style';
+import {
+  Card, Container, Header, ListContainer,
+} from './style';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export function ContactsList() {
   return (
@@ -7,6 +13,36 @@ export function ContactsList() {
         <strong>ContactsList</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            Nome
+            <img src={arrow} alt="Arrow Icon" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Fiama de Capua</strong>
+              <small>instagram</small>
+            </div>
+            <span>email@emaildev.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
