1bf541782be52b44d41da332d42bd8a2fbcdadb4
FihCapua <fihcapua@gmail.com>
2023-05-17 21:25:33 -0300

    feat: create Input and Select component

---
+++ src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+  font-size: 16px;
+  background: #FFF;
+  box-shadow: 0px 4px 10px rgba(0,0,0,0.4);
+  border: 2px solid #fff;
+  border-radius: 4px;
+  padding: 0 16px;
+  outline: none;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main}
+  }
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,16 @@
+import Input from '../../components/Input';
 import { PageHeader } from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
