f2e2e340cfe638efc8585953db6b00a1b36a325b
FihCapua <fihcapua@gmail.com>
2023-05-21 16:30:45 -0300

    feat: handle submit form contact and treat data values

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ButtonContainer, Form } from './style';
@@ -8,28 +9,47 @@ import Select from '../Select';
 import Button from '../Button';
 
 export function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup error="E-mail inválido">
-        <Input placeholder="Email" error />
+        <Input placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input placeholder="Telefone" value={phone} onChange={(event) => setPhone(event.target.value)} />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select value={category} onChange={(event) => setCategory(event.target.value)}>
           <option value="instagram">Instagram</option>
+          <option value="instagram">Linkedin</option>
+          <option value="instagram">Github</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="button">{buttonLabel}</Button>
+        <Button type="submit">{buttonLabel}</Button>
       </ButtonContainer>
     </Form>
   );
