803731148ee4458f201199e1ef94b6d9ca80ed27
FihCapua <fihcapua@gmail.com>
2023-06-05 21:41:32 -0300

    feat: create httpClient and abstract api path

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,7 +1,12 @@
+import HttpClient from './utils/HttpClient';
+
 class ContactsService {
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
+
   async listContacts(orderBy = 'asc') {
-    const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
-    return response.json();
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 
