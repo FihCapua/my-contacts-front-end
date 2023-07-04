a51cc81fa6cdb6e716b074787e8a53d1fee1d1fb
FihCapua <fihcapua@gmail.com>
2023-07-03 22:22:28 -0300

    feat: display message from no registered contact

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable-next-line max-len */
+/* eslint-disable no-nested-ternary */
+
 import {
   useCallback, useEffect, useMemo, useState,
 } from 'react';
@@ -9,12 +13,14 @@ import {
   Header,
   ListHeader,
   ErrorContainer,
+  EmptyListContainer,
 } from './style';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
+import emptyBox from '../../assets/images/empty-box.svg';
 
 import { Loader } from '../../components/Loader';
 import Button from '../../components/Button';
@@ -27,7 +33,6 @@ export function Home() {
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
 
-  // eslint-disable-next-line max-len
   const filteredSearchTheme = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
@@ -66,17 +71,31 @@ export function Home() {
   return (
     <Container>
       <Loader isLoading={isLoading} />
-      <InputSearchContainer>
-        <input
-          value={searchTerm}
-          type="text"
-          placeholder="Pesquisar pelo nome..."
-          onChange={handleSearchTerm}
-        />
-      </InputSearchContainer>
 
-      <Header haserror={hasError.toString()}>
-        {!hasError && (
+      {contacts.length > 0 && (
+        <InputSearchContainer>
+          <input
+            value={searchTerm}
+            type="text"
+            placeholder="Pesquisar pelo nome..."
+            onChange={handleSearchTerm}
+          />
+        </InputSearchContainer>
+      )}
+
+      <Header
+        justifycontent={
+          hasError
+            ? 'flex-end'
+            : (
+              contacts.length > 0
+                ? 'space-between'
+                : 'center'
+            )
+        }
+        haserror={hasError.toString()}
+      >
+        {(!hasError && contacts.length > 0) && (
           <strong>
             {filteredSearchTheme.length}
             {filteredSearchTheme.length === 1 ? ' contato' : ' contatos'}
@@ -97,6 +116,17 @@ export function Home() {
 
       {!hasError && (
         <>
+          {(contacts.length < 1 && !isLoading) && (
+            <EmptyListContainer>
+              <img src={emptyBox} alt="Empty Box" />
+
+              <p>
+                Você ainda não tem nenhum contato cadastrado!
+                Clique no botão <strong>&ldquo;Novo contato &rdquo;</strong> à cima
+                para cadastrar o seu primeiro!
+              </p>
+            </EmptyListContainer>
+          )}
           {filteredSearchTheme.length > 0 && (
           <ListHeader orderby={orderBy}>
             <button type="button" onClick={handleToggleOrder}>
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -25,7 +25,7 @@ export const InputSearchContainer = styled.div`
 
 export const Header = styled.header`
   display: flex;
-  justify-content: ${({ hasError }) => (hasError ? 'flex-end' : 'space-between')};
+  justify-content: ${({ justifycontent }) => (justifycontent)};
   align-items: center;
   margin-top: 32px;
   border-bottom: 2px solid ${({ theme }) => theme.colors.gray[100]};
@@ -139,6 +139,23 @@ export const ErrorContainer = styled.div`
       color: ${({ theme }) => theme.colors.danger.main};
       display: block;
       margin-bottom: 8px;
+    }
+  }
+`;
+
+export const EmptyListContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    color: ${({ theme }) => theme.colors.gray[200]};
+    text-align: center;
+    margin-top: 16px;
+
+    strong {
+      color: ${({ theme }) => theme.colors.primary.main}
     }
   }
 `;
